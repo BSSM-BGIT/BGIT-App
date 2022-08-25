fa4296a5a7a044173cfc208f3ee01a93a1d0c627
조용제 <whehdrms6810@naver.com>
2022-08-25 11:19:17 +0900

    [CREATE] commom.dart

---
+++ bssm_app/lib/common/common.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class CommonColor {
+  static Color background = const Color(0xFFEAEEF1);
+  static Color commitText = const Color(0xFF91D1D3);
+  static Color text = const Color(0xFF7D8AA5);
+}
