446fbbec091e2cc589d92652b52bf3f346928ff7
조용제 <whehdrms6810@naver.com>
2022-08-25 11:19:30 +0900

    [CREATE] home.dart

---
+++ bssm_app/lib/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text("home", style: TextStyle(fontSize: 50.sp),)
+        ],
+      ),
+    );
+  }
+}
