9db52447d7410f825589a220caf61631ddcace51
조용제 <whehdrms6810@naver.com>
2022-08-28 21:55:17 +0900

    [UPDATE] bottombar delete

--- bssm_app/lib/main.dart
+++ bssm_app/lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:bssm_app/screens/home.dart';
 import 'package:bssm_app/screens/rank.dart';
-import 'package:bssm_app/screens/user.dart';
-import 'package:bssm_app/widgets/bottombar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -15,21 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: [SystemUiOverlay.bottom]);
+        overlays: [SystemUiOverlay.top]);
     return ScreenUtilInit(
-      designSize: const Size(414, 736),
-      builder: (BuildContext context, Widget? child) => MaterialApp(
-        title: 'BSSM_Git',
-        debugShowCheckedModeBanner: false,
-        home: DefaultTabController(
-            length: 3,
-            child: Scaffold(
-                body: const TabBarView(
-                  physics: NeverScrollableScrollPhysics(),
-                  children: [Home(), Rank(), User()],
-                ),
-                bottomNavigationBar: Bottom.bottomBar())),
-      ),
-    );
+        designSize: const Size(414, 736),
+        builder: (BuildContext context, Widget? child) => const MaterialApp(
+            title: 'BSSM_Git',
+            debugShowCheckedModeBanner: false,
+            home: DefaultTabController(
+                length: 3, child: Scaffold(body: Rank()))));
   }
 }
