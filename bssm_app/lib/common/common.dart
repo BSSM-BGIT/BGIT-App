24250f0df04e3e0c22130c040a61cf60c9b3037b
조용제 <whehdrms6810@naver.com>
2022-08-28 21:55:33 +0900

    [UPDATE] purple1, purple2 color

--- bssm_app/lib/common/common.dart
+++ bssm_app/lib/common/common.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CommonColor {
-  static Color purple1 = const Color(0xFF312F69);
-  static Color purple2 = const Color(0xFF211E68);
+  static Color purple1 = const Color(0xFF3D3380);
+  static Color purple2 = const Color(0xFF21206C);
   static Color purple3 = const Color(0xFF566383);
 }
