8558e0728cdca724c2bdf389a96b38657ca78540
조용제 <whehdrms6810@naver.com>
2022-08-30 20:27:17 +0900

    [CREATE] webview.dart

---
+++ bssm_app/lib/screens/webview.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class Webview extends StatefulWidget {
+  const Webview({Key? key}) : super(key: key);
+
+  @override
+  State<Webview> createState() => _WebviewState();
+}
+
+class _WebviewState extends State<Webview> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: WebView(
+        initialUrl:
+            'https://bssm.kro.kr/oauth/login?clientId=4c81669f&redirectURI=http://localhost:8080/auth/oauth/bsm',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          _controller.complete(webViewController);
+        },
+      ),
+    );
+  }
+}
