9ca514da87409275c638da560d5c46f7ed38c9d5
조용제 <whehdrms6810@naver.com>
2022-08-30 20:26:33 +0900

    [ADD] dialog

--- bssm_app/lib/screens/rank.dart
+++ bssm_app/lib/screens/rank.dart
@@ -1,8 +1,12 @@
 import 'package:bssm_app/common/common.dart';
+import 'package:bssm_app/screens/webview.dart';
 import 'package:bssm_app/widgets/rank_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:flutter_animated_dialog/flutter_animated_dialog.dart';
+import 'package:material_dialogs/material_dialogs.dart';
 
 class Rank extends StatefulWidget {
   const Rank({Key? key}) : super(key: key);
@@ -18,12 +22,79 @@ class _RankState extends State<Rank> {
 
   int rank = 1;
   late int rankColor;
+  bool registration = false;
 
-
-@override
+  @override
   void initState() {
     super.initState();
     initialization();
+  }
+
+  void showDialog() {
+    // showAnimatedDialog(
+    //   context: context,
+    //   barrierDismissible: true,
+    //   builder: (BuildContext context) {
+    //     return
+    //   },
+    //   animationType: DialogTransitionType.slideFromBottomFade,
+    //   curve: Curves.fastOutSlowIn,
+    //   duration: const Duration(seconds: 1),
+    // );
+    Dialogs.materialDialog(
+      color: Colors.white,
+      msg: '간편하게 로그인 해보세요',
+      title: 'github 로그인',
+      lottieBuilder: Lottie.asset(
+        'assets/dialog.json',
+        fit: BoxFit.contain,
+      ),
+      context: context,
+      actions: [
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.white),
+          child: Padding(
+            padding: EdgeInsets.only(left: 10.w, right: 10.w),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              
+              Text(
+                "취소",
+                style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w500),
+              )
+            ]),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+             Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const Webview()));
+          },
+          style: ElevatedButton.styleFrom(primary: Colors.blue),
+          child: Padding(
+            padding: EdgeInsets.only(left: 10.w, right: 10.w),
+            child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const Icon(
+                Icons.check,
+                color: Colors.white,
+              ),
+              Text(
+                "확인",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w500),
+              )
+            ]),
+          ),
+        ),
+      ],
+    );
   }
 
   void initialization() async {
@@ -66,6 +137,22 @@ class _RankState extends State<Rank> {
               color: Colors.white),
         ),
         backgroundColor: CommonColor.purple4,
+        actions: [
+          Padding(
+              padding: EdgeInsets.only(top: 12.h, bottom: 12.h, right: 8.w),
+              child: ElevatedButton(
+                  onPressed: () {
+                    showDialog();
+                  },
+                  style: ElevatedButton.styleFrom(),
+                  child: Text(
+                    "등록",
+                    style: TextStyle(
+                        fontSize: 18.sp,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w500),
+                  )))
+        ],
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
