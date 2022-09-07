162ecdc3f3a0e7308b28b39d2e611e8a84d1586a
조용제 <whehdrms6810@naver.com>
2022-09-08 00:45:03 +0900

    [ADD] bottombar

--- bssm_app/lib/main.dart
+++ bssm_app/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:bssm_app/screens/community.dart';
 import 'package:bssm_app/screens/rank.dart';
+import 'package:bssm_app/screens/user.dart';
+import 'package:bssm_app/widgets/bottombar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 
@@ -11,16 +14,31 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
-        overlays: [SystemUiOverlay.top]);
+        overlays: [SystemUiOverlay.bottom]);
     return ScreenUtilInit(
-        designSize: const Size(414, 736),
-        builder: (BuildContext context, Widget? child) => const MaterialApp(
-            title: 'BSSM_Git',
-            debugShowCheckedModeBanner: false,
-            home: Rank()));
+      designSize: const Size(414, 736),
+      builder: (BuildContext context, Widget? child) => const MaterialApp(
+        title: 'BSSM_Git',
+        debugShowCheckedModeBanner: false,
+        home: Rank()
+        // DefaultTabController(
+        //     length: 3,
+        //     child: Scaffold(
+        //         body: const TabBarView(
+        //           physics: NeverScrollableScrollPhysics(),
+        //           children: [Rank(), Community(), User()],
+        //         ),
+        //         bottomNavigationBar: Bottom.bottomBar())),
+      ),
+    );
   }
 }
