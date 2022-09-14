f4bff7f5cba22e3252e23fc0349451c2d0e8c71d
조용제 <whehdrms6810@naver.com>
2022-09-14 20:28:44 +0900

    [CREATE] community.dart

---
+++ bssm_app/lib/screens/bottombar/community.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Community extends StatelessWidget {
+  const Community({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: const [
+          Text("커뮤니티", style: TextStyle(fontSize: 35),)
+        ],
+      ),
+    );
+  }
+}
