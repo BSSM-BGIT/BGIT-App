3e9e45071a8d2d3f13fe4cf43465ffbb77c495cc
조용제 <whehdrms6810@naver.com>
2022-09-15 13:36:54 +0900

    [CREATE] glist.dart

---
+++ bssm_app/lib/model/glist.dart
@@ -0,0 +1,9 @@
+class GitRanklist {
+  int rank;
+  String profile;
+  String userName;
+  String name;
+  int commit;
+
+  GitRanklist(this.rank, this.profile, this.userName, this.name, this.commit);
+}
