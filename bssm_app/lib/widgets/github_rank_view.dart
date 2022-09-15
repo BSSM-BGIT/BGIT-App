ec19e0170ab8711359f8315b8b321ae6dde17c2b
조용제 <whehdrms6810@naver.com>
2022-09-15 13:37:25 +0900

    [CREATE] github_rank_view.dart

---
+++ bssm_app/lib/widgets/github_rank_view.dart
@@ -0,0 +1,138 @@
+import 'package:bssm_app/common/common.dart';
+import 'package:bssm_app/model/glist.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class Rankview extends StatefulWidget {
+  final int myRank;
+  const Rankview({Key? key, required this.myRank}) : super(key: key);
+
+  @override
+  State<Rankview> createState() => _RankviewState();
+}
+
+class _RankviewState extends State<Rankview> {
+  List<GitRanklist> ranks = <GitRanklist>[];
+
+  int count = 9600;
+
+  @override
+  void initState() {
+    super.initState();
+    for (int i = 4; i <= 9; i++) {
+      ranks.add(GitRanklist(i, "profile", "userName$i", "name", count));
+      count -= 100;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: makeRankList(context, ranks, widget.myRank),
+    );
+  }
+}
+
+List<Widget> makeRankList(
+    BuildContext context, List<GitRanklist> ranks, int myRank) {
+  List<Widget> results = [];
+
+  for (var i = 0; i < ranks.length; i++) {
+    int rankcolor = colorSelect(ranks[i].rank);
+    results.add(Padding(
+      padding: EdgeInsets.only(left: 20.w, right: 20.w, top: 13.h),
+      child: Container(
+        width: double.infinity,
+        height: 75.h,
+        decoration: BoxDecoration(
+          color: ranks[i].rank == myRank
+              ? Color.fromARGB(255, 175, 177, 179).withOpacity(0.4)
+              : Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(20.r)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.3),
+              spreadRadius: 2,
+              blurRadius: 7,
+              offset: Offset(0, 2.h), // changes position of shadow
+            ),
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+                padding: EdgeInsets.only(
+                    left: 23.w, right: 23.w, top: 10.h, bottom: 10.h),
+                child: Text(
+                  "${ranks[i].rank}",
+                  style: TextStyle(
+                      color: CommonColor.blue,
+                      fontSize: 30.sp,
+                      fontWeight: FontWeight.w500),
+                )),
+            Container(
+              margin: EdgeInsets.only(bottom: 5.h, right: 8.w),
+              height: 40.h,
+              width: 40.w,
+              decoration: const BoxDecoration(
+                  shape: BoxShape.circle, color: Colors.black),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 5.h),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 2.h),
+                    child: Text(
+                      ranks[i].userName,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17.sp,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                  Text(
+                    ranks[i].name,
+                    style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 15.sp,
+                        fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+                padding: EdgeInsets.only(left: 75.w, right: 2.w),
+                child: const Icon(Icons.check_box_outlined)),
+            Text(
+              "${ranks[i].commit}",
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 17.sp,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+  return results;
+}
+
+int colorSelect(int rank) {
+  int rankColor;
+  if (rank == 1) {
+    rankColor = 0xffFF3B5A;
+  } else if (rank == 2) {
+    rankColor = 0xffFF6A00;
+  } else if (rank == 3) {
+    rankColor = 0xffE6DA44;
+  } else {
+    rankColor = 0xffFFFFFFF;
+  }
+  return rankColor;
+}
