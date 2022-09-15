a7a2049805510673cb84cba4259ea87aa1605532
조용제 <whehdrms6810@naver.com>
2022-09-15 13:36:42 +0900

    [CREATE] blist.dart

---
+++ bssm_app/lib/model/blist.dart
@@ -0,0 +1,10 @@
+class BaekjoonRanklist {
+  int rank;
+  String profile;
+  String userName;
+  String name;
+  int solve;
+  String tear;
+
+  BaekjoonRanklist(this.rank, this.profile, this.userName, this.name, this.solve,this.tear);
+}
