ca0aed5579169d9e6cf49398506a94d19e722ff6
조용제 <whehdrms6810@naver.com>
2022-09-24 23:50:50 +0900

    [CREATE] clist.dart

---
+++ bssm_app/lib/model/clist.dart
@@ -0,0 +1,10 @@
+
+class Communitylist {
+  String title;
+  String content;
+  int views;
+  int likes;
+  int minutes;
+
+  Communitylist(this.title, this.content, this.views, this.likes, this.minutes);
+}
