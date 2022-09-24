e969277049ad1d46d9f61cf11e8833d25d13ea87
조용제 <whehdrms6810@naver.com>
2022-09-24 23:51:44 +0900

    [ADD] navigator.push

--- bssm_app/lib/screens/bottombar/user.dart
+++ bssm_app/lib/screens/bottombar/user.dart
@@ -1,3 +1,4 @@
+import 'package:bssm_app/screens/webview.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:flutter_web_auth/flutter_web_auth.dart';
@@ -10,29 +11,6 @@ class User extends StatefulWidget {
 }
 
 class _UserState extends State<User> {
-  Future<void> signIn() async {
-    
-    // 고유한 redirect uri
-    const APP_REDIRECT_URI = "com.example.bssm_app";
-    
-    // 백엔드에서 미리 작성된 API 호출
-    final url = Uri.parse('');
-    
-    // 백엔드가 제공한 로그인 페이지에서 로그인 후 callback 데이터 반환
-    final result = await FlutterWebAuth.authenticate(
-        url: url.toString(), callbackUrlScheme: APP_REDIRECT_URI);
-    
-    // 백엔드에서 redirect한 callback 데이터 파싱
-    final accessToken = Uri.parse(result).queryParameters['access-token'];
-    final refreshToken = Uri.parse(result).queryParameters['refresh-token'];
-    
-    // . . .
-    // FlutterSecureStorage 또는 SharedPreferences 를 통한
-    // Token 저장 및 관리
-    // . . .
-    
-}
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +20,8 @@ class _UserState extends State<User> {
           children: [
             ElevatedButton(
                 onPressed: () {
-                  signIn();
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (_) => const Webview()));
                 },
                 child: const Text(
                   "로그인",
