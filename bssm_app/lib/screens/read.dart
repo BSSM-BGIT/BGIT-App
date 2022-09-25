70698803e4106706bc8ce4359f166a6aafa36ebb
조용제 <whehdrms6810@naver.com>
2022-09-25 21:22:47 +0900

    [CREATE] read.dart

---
+++ bssm_app/lib/screens/read.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Read extends StatefulWidget {
+  const Read({super.key});
+
+  @override
+  State<Read> createState() => _ReadState();
+}
+
+class _ReadState extends State<Read> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
