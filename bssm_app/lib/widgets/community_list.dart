3798b93a47e2364f149b6ee4709faaa46334b1c2
조용제 <whehdrms6810@naver.com>
2022-09-25 21:22:22 +0900

    [UPDATE] ui

--- bssm_app/lib/widgets/community_list.dart
+++ bssm_app/lib/widgets/community_list.dart
@@ -1,5 +1,7 @@
 import 'package:bssm_app/model/clist.dart';
+import 'package:bssm_app/screens/read.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CommunityList extends StatefulWidget {
   const CommunityList({super.key});
@@ -9,7 +11,7 @@ class CommunityList extends StatefulWidget {
 }
 
 class _CommunityListState extends State<CommunityList> {
-  List<Communitylist> community = <Communitylist>[];
+  List<Communitylist> lists = <Communitylist>[];
   int a = 16;
   int minutes = 1;
   @override
@@ -17,15 +19,107 @@ class _CommunityListState extends State<CommunityList> {
     super.initState();
 
     for (int i = 1; i <= 15; i++) {
-      community.add(Communitylist("title$i", "$i번째 내용입니다", a, a, minutes));
+      lists.add(Communitylist("$i번째 title입니다", "$i번째 내용입니다", a, a, minutes));
       a = a - 1;
       minutes += 1;
     }
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      
+      children: makeCommunityList(context, lists),
     );
   }
+}
+
+List<Widget> makeCommunityList(
+    BuildContext context, List<Communitylist> lists) {
+  List<Widget> results = [];
+
+  for (var i = 0; i < lists.length; i++) {
+    results.add(
+      InkWell(
+        onTap: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => const Read()));
+        },
+        child: Container(
+          width: double.infinity,
+          height: 80.h,
+          decoration: BoxDecoration(
+            color: Colors.transparent,
+            border: Border.all(
+              width: 0.1.w,
+              color: Colors.black.withOpacity(0.4),
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 16.h,
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: Text(
+                  lists[i].title,
+                  style:
+                      TextStyle(fontSize: 21.sp, fontWeight: FontWeight.w500),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 20.w),
+                child: Text(
+                  lists[i].content,
+                  style:
+                      TextStyle(fontSize: 14.sp, fontWeight: FontWeight.w400),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 20.w),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Icon(
+                      Icons.favorite_border,
+                      color: Colors.black,
+                      size: 20.r,
+                    ),
+                    Text(
+                      "${lists[i].likes}",
+                      style: TextStyle(fontSize: 16.sp),
+                    ),
+                    SizedBox(
+                      width: 20.w,
+                    ),
+                    Icon(
+                      Icons.visibility_outlined,
+                      color: Colors.black,
+                      size: 20.r,
+                    ),
+                    Text(
+                      "${lists[i].views}",
+                      style: TextStyle(fontSize: 16.sp),
+                    ),
+                    SizedBox(
+                      width: 20.w,
+                    ),
+                    Text(
+                      "${lists[i].views}분전",
+                      style: TextStyle(
+                          color: Colors.black.withOpacity(0.5),
+                          fontWeight: FontWeight.w400),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  return results;
 }
