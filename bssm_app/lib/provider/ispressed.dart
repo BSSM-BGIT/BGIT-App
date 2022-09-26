eb2c887f163e73ad700d09625e1a08c8385f136d
조용제 <whehdrms6810@naver.com>
2022-09-26 21:24:51 +0900

    [CREATE] ispressed.dart

---
+++ bssm_app/lib/provider/ispressed.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Pressed extends ChangeNotifier {
+  // ChangeNotifier를 extends한다.
+  bool _bsmispressed = false;
+  bool _githubispressed = false;
+  String _accessToken = "";
+
+  String get accessToken => _accessToken;
+  bool get bsmispressed => _bsmispressed;
+  bool get githubispressed => _githubispressed;
+
+  void bsmchange() {
+    _bsmispressed = true;
+    print("실행됨");
+    notifyListeners();
+  }
+
+  void githubchange() {
+    _githubispressed = true;
+    notifyListeners();
+  }
+
+  void inputaccesstoken(String access) {
+    print("access = $access");
+
+    _accessToken = access;
+    print("accessToken = $accessToken");
+    notifyListeners();
+  }
+}
