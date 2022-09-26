cedd2ee3e8405fb092133a912a0834537b9aa78f
조용제 <whehdrms6810@naver.com>
2022-09-26 21:23:52 +0900

    [ADD] login success

--- bssm_app/lib/screens/bottombar/user.dart
+++ bssm_app/lib/screens/bottombar/user.dart
@@ -1,7 +1,8 @@
-import 'package:bssm_app/screens/webview.dart';
+import 'package:bssm_app/provider/ispressed.dart';
+import 'package:bssm_app/screens/bsm/webview.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:flutter_web_auth/flutter_web_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class User extends StatefulWidget {
   const User({Key? key}) : super(key: key);
@@ -13,23 +14,36 @@ class User extends StatefulWidget {
 class _UserState extends State<User> {
   @override
   Widget build(BuildContext context) {
+    var pressed = Provider.of<Pressed>(context);
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => const Webview()));
-                },
-                child: const Text(
-                  "로그인",
-                  style: TextStyle(fontSize: 20),
-                ))
-          ],
-        ),
-      ),
+      body: pressed.bsmispressed
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "로그인 완료",
+                    style: TextStyle(fontSize: 20.sp, color: Colors.black),
+                  )
+                ],
+              ),
+            )
+          : Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (_) => const Webview()));
+                      },
+                      child: const Text(
+                        "로그인",
+                        style: TextStyle(fontSize: 20),
+                      ))
+                ],
+              ),
+            ),
     );
   }
 }
