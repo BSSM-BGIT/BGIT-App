11bc5c5a9ec5ab67ccbbf6e325bd037411fc8328
조용제 <whehdrms6810@naver.com>
2022-09-28 21:17:59 +0900

    [UPDATE] url

--- bssm_app/lib/screens/bsm/webview.dart
+++ bssm_app/lib/screens/bsm/webview.dart
@@ -21,7 +21,7 @@ class _WebviewState extends State<Webview> {
   bool close = false;
   bool postend = false;
   String code = "";
-  String url = "http://localhost:3000/oauth/bsm?code=";
+  String url = "https://bgit.bssm.kro.kr/oauth/bsm";
   String accessToken = "";
   final Completer<WebViewController> _controller =
       Completer<WebViewController>();
@@ -42,8 +42,7 @@ class _WebviewState extends State<Webview> {
     accessToken = bsmData['accessToken'];
     print("22222222222222222221222222222222222222222222222222222222");
     print(accessToken);
-    pressed =pressed.inputaccesstoken(accessToken);
-
+    pressed = pressed.inputaccesstoken(accessToken);
 
     // http.Response response = await http.post(
     //   Uri.parse(url),
@@ -67,23 +66,26 @@ class _WebviewState extends State<Webview> {
           ? Center(child: SpinKitWave(color: CommonColor.blue))
           : WebView(
               initialUrl:
-                  'https://bssm.kro.kr/oauth/login?clientId=4c81669f&redirectURI=http://localhost:3000/oauth/bsm',
+                  'https://bssm.kro.kr/oauth/login?clientId=4c81669f&redirectURI=https://bgit.bssm.kro.kr/oauth/bsm',
               javascriptMode: JavascriptMode.unrestricted,
               navigationDelegate: (NavigationRequest request) {
-                if (request.url.contains(url)) {
+                if (request.url.contains("?code=")) {
                   setState(() {
                     code = request.url.split("?code=")[1];
                     close = true;
 
                     _postRequest(pressed);
+
                     
-                    pressed.bsmchange();
-                    
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (_) => const BsmSuccess()));
                   });
                   // do not navigate
                   return NavigationDecision.prevent;
+                } else if (request.url.contains(url)) {
+                  pressed.bsmchange();
+
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (_) => const BsmSuccess()));
+
                 }
 
                 return NavigationDecision.navigate;
