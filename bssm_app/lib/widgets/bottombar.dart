5056bf91ec9b7da8ea4b8fa18e3eaa6ef9cd8f55
조용제 <whehdrms6810@naver.com>
2022-10-10 20:33:18 +0900

    [DELETE] common import

--- bssm_app/lib/widgets/bottombar.dart
+++ bssm_app/lib/widgets/bottombar.dart
@@ -1,4 +1,3 @@
-import 'package:bssm_app/common/common.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
