1e6b78450dc2de088bf9327d385ececc8acbaffb
조용제 <whehdrms6810@naver.com>
2022-10-13 22:27:48 +0900

    [DELETE] http

--- bssm_app/lib/data/network.dart
+++ bssm_app/lib/data/network.dart
@@ -1,5 +1,4 @@
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+
 
 class Network {
   final String url;
@@ -7,15 +6,7 @@ class Network {
   Network(this.url, this.code);
 
   Future<dynamic> getJsonData() async {
-    http.Response response =
-        await http.post(Uri.parse(url), headers: <String, String>{
-      'authCode': code,
-    });
-    if (response.statusCode == 200) {
-      String jsonData = response.body;
-      var parsingData = jsonDecode(jsonData);
-      return parsingData;
-    }
+    
   }
 
   
