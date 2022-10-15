244c19681596af64ec3ddce6409c10dea30f4436
조용제 <whehdrms6810@naver.com>
2022-10-15 23:10:28 +0900

    [ADD] post http

--- bssm_app/lib/screens/bsm/bsm_loginsuccess.dart
+++ bssm_app/lib/screens/bsm/bsm_loginsuccess.dart
@@ -3,6 +3,8 @@ import 'package:bssm_app/widgets/loading.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class BsmSuccess extends StatefulWidget {
   const BsmSuccess({super.key});
@@ -17,10 +19,36 @@ class _BsmSuccessState extends State<BsmSuccess> {
     super.initState();
   }
 
+  Future<void> _getRequest(var access, var pressed) async {
+    String url = 'http://52.79.57.84:8080/user';
+    print("실행됨");
+    print(access);
+    http.Response response =
+        await http.get(Uri.parse(url), headers: <String, String>{
+      'ACCESS-TOKEN': access,
+    });
+    if (response.statusCode == 200) {
+      print("성공함");
+      var parsingData = jsonDecode(utf8.decode(response.bodyBytes));
+      print(parsingData);
+      if (parsingData['githubAuth']) {
+        pressed.gitchange();
+      }
+      if (parsingData['bojAuth']) {
+        pressed.bojchange();
+      }
+      Navigator.push(
+          context, MaterialPageRoute(builder: (_) => const Loading()));
+    } else {
+      print("실패함");
+      //오류 발생 코드
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    var pressed = Provider.of<Pressed>(context, listen: false);
     // ignore: unused_local_variable
-    var pressed = Provider.of<Pressed>(context);
     return Scaffold(
       body: Center(
         child: Column(
@@ -35,8 +63,7 @@ class _BsmSuccessState extends State<BsmSuccess> {
             ),
             ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => const Loading()));
+                  _getRequest(pressed.accessToken, pressed);
                 },
                 child: Text(
                   "돌아가기",
